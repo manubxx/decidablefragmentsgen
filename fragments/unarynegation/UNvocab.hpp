49da4777ef9488c1b4be159742fc3015e05ed62d
Manuele Borgia <borgiamanuele@gmail.com>
2026-06-06 20:56:40 +0200

    Unary Negation Fragment default vocabulary

---
+++ fragments/unarynegation/UNvocab.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../../FragmentTypes.hpp"
+#include <vector>
+
+// UN Fragment Default Vocabulary
+inline const std::vector<PredInfo> kVocabUN = {
+    { "A0", 1 },
+
+    { "B0", 2 },
+    { "B1", 2 },
+
+    { "C0", 3 },
+    { "C1", 3 },
+
+    { "D0", 4 },
+    { "D1", 4 },
+    { "D2", 4 }
+};
